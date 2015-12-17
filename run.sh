f96cc3ef26914020639f8d09e56cfb76a3527bb0
Lukas Martinelli <me@lukasmartinelli.ch>
2015-12-17 14:57:05 +0100

    Remove X-Ray fallback

--- run.sh
+++ run.sh
@@ -12,14 +12,6 @@ readonly PORT=${PORT:-80}
 readonly CACHE_SIZE=${CACHE_SIZE:-10}
 readonly SOURCE_CACHE_SIZE=${SOURCE_CACHE_SIZE:-10}
 
-function serve_xray() {
-    local mbtiles_file=$1
-    exec bin/tessera.js "xray+mbtiles://$mbtiles_file" \
-        --PORT $PORT \
-        --cache-size $CACHE_SIZE \
-        --source-cache-size $SOURCE_CACHE_SIZE
-}
-
 function find_first_mbtiles() {
     for mbtiles_file in "$SOURCE_DATA_DIR"/*.mbtiles; do
         echo "${mbtiles_file}"
@@ -101,14 +93,9 @@ function serve() {
     echo "------- Find additional information on how to use this container under the following link: http://osm2vectortiles.org/docs/start/"
     if [ -f "$mbtiles_file" ]; then
         echo "Using $mbtiles_file as vector tile source"
-        if [ -d "$tm2project" ]; then
-            replace_sources "$mbtiles_file"
-            create_tessera_config "$mbtiles_file"
-            serve_config
-        else
-            echo "The mbtiles file is now served with X-Ray styles"
-            serve_xray "$mbtiles_file"
-        fi
+        replace_sources "$mbtiles_file"
+        create_tessera_config "$mbtiles_file"
+        serve_config
     else
         # Serve empty config
         rm -f "$TESSERA_CONFIG"
